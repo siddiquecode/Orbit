83a58e01035dcb4c9cf240eaccc2d64397a1bf68
Muhammed siddique <siddiquehussain2002@gmail.com>
2024-11-07 18:00:32 +0530

    google

--- connection/connection.js
+++ connection/connection.js
@@ -3,7 +3,8 @@ const mongoose=require('mongoose')
 const connectDB=async()=>{
     try{
         console.log('MongoDB has connected')
-        const connection=await mongoose.connect('mongodb://127.0.0.1:27017/orbit',{})
+        // const connection=await mongoose.connect('mongodb://127.0.0.1:27017/orbit',{})
+        const connection=await mongoose.connect('mongodb+srv://siddiquehussain2002:VggxDuRQDiwNBymF@orbit.zc8px.mongodb.net/',{})
     }catch(err){
         console.log('MongoDB has not connected')
         console.log(err)
