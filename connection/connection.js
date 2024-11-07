93192884989fe94e627fcc5671377ad785402f7e
Muhammed siddique <siddiquehussain2002@gmail.com>
2024-11-07 18:18:29 +0530

    google auth is fixd

--- connection/connection.js
+++ connection/connection.js
@@ -4,7 +4,7 @@ const connectDB=async()=>{
     try{
         console.log('MongoDB has connected')
         // const connection=await mongoose.connect('mongodb://127.0.0.1:27017/orbit',{})
-        const connection=await mongoose.connect('mongodb+srv://siddiquehussain2002:VggxDuRQDiwNBymF@orbit.zc8px.mongodb.net/OrbitDB?retryWrites=true&w=majority',{})
+        const connection=await mongoose.connect('mongodb+srv://siddiquehussain2002:VggxDuRQDiwNBymF@orbit.zc8px.mongodb.net/Orbit?retryWrites=true&w=majority',{})
     }catch(err){
         console.log('MongoDB has not connected')
         console.log(err)
