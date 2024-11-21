9324a72400116deadf31d604e18ff2377e645bb3
siddique hussain <siddiquehussain2002@gmail.com>
2024-11-21 14:26:37 +0530

    coupon issue solved

--- controller/admin_controller/couponCtrl.js
+++ controller/admin_controller/couponCtrl.js
@@ -58,6 +58,14 @@ const editCoupon_Post = async (req, res) => {
     const { couponCode, discount, minimumAmount, maximumAmount, expireDate } =
       req.body;
 
+    await couponDB.findByIdAndUpdate(couponId, {
+      couponCode,
+      discount,
+      minimumAmount,
+      maximumAmount,
+      expireDate,
+    });
+
     res.redirect("/admin/getcoupon");
   } catch (error) {
     console.log(error);
@@ -67,6 +75,7 @@ const editCoupon_Post = async (req, res) => {
 const deleteCoupon = async (req, res) => {
   try {
     const couponId = req.params.id;
+    await couponDB.findByIdAndDelete(couponId);
     res.status(200).json({ message: "Coupon deleted successfully" });
   } catch (error) {
     console.log(error);
